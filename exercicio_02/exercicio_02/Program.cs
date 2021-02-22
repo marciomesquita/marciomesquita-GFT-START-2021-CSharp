33c8082a3d40e22b373aad4d7352b0784b91eb54
Marcio Mesquita <botasebatatas@gmail.com>
2021-02-22 09:57:01 -0300

    Exercicio 02

---
+++ exercicio_02/exercicio_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o intervalo inicial: ");
+            int intervaloInicial = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o intervalo final: ");
+            int intervaloFinal= Convert.ToInt32(Console.ReadLine());
+
+            if(intervaloFinal - intervaloInicial < 15)
+            {
+                for (int i = intervaloInicial; i <= intervaloFinal; i++)
+                {
+                    var cuboDoNumero = Math.Pow(i, 3);
+                    Console.WriteLine($"Cubo de {i}:{cuboDoNumero}");
+                }
+            }
+
+            
+
+
+
+        }
+    }
+}
