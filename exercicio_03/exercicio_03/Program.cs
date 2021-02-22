e93e53570965b8cc69bb75bcd56a22f425ddd622
Marcio Mesquita <botasebatatas@gmail.com>
2021-02-22 10:07:33 -0300

    Exercicio 03

---
+++ exercicio_03/exercicio_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int[] divisores = new int[] {3,6,7};
+
+            Console.Write("Divisível por ");
+
+            foreach(int divisor in divisores)
+            {
+                if (numero % divisor == 0)
+                {
+                    Console.Write($"{divisor} ");
+                }
+
+                
+            }
+
+        }
+    }
+}
